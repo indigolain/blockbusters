c9e7cbe1cc4cfee095ffe6073d61ab73d6eff3a3
indigolain <indigo.lain@gmail.com>
2019-09-16 04:12:58 +0900

    Add spec for testing out order of movies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,4 +18,18 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include JsonSpec::Helpers
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
--- spec/requests/movies_controller_spec.rb
+++ spec/requests/movies_controller_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe MoviesController, type: :request do
   describe 'GET #index' do
     subject(:get_request) { get movies_path, headers: request_header }
 
+    let(:old_movie) { create(:movie, created_at: 1.day.ago) }
+    let(:new_movie) { create(:movie, created_at: Time.current) }
+
+    before do
+      new_movie
+      old_movie
+      get_request
+    end
+
     it { is_expected.to eq 200 }
+    it 'returns list of movies ordered by creation' do
+      expect(parse_json(response.body, 'data/0/id')).to eq(old_movie.id.to_s)
+      expect(parse_json(response.body, 'data/1/id')).to eq(new_movie.id.to_s)
+    end
   end
 end

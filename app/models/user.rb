e25ffa4ad4f90b449accc4818b317c14a0a7c94a
indigolain <indigo.lain@gmail.com>
2019-09-15 23:27:38 +0900

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
+class User < ApplicationRecord
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+end

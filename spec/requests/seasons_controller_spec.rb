f625ec727b8a2519cffe5405599ea78ad8662ce5
indigolain <indigo.lain@gmail.com>
2019-09-16 04:24:21 +0900

    Add spec for testing out order of episodes

--- spec/requests/seasons_controller_spec.rb
+++ spec/requests/seasons_controller_spec.rb
@@ -10,6 +10,39 @@ RSpec.describe SeasonsController, type: :request do
       get seasons_path, headers: request_header
     }
 
+    let(:old_episode) {
+      create(:episode, created_at: 1.day.ago)
+    }
+    let(:new_episode) {
+      create(:episode, created_at: Time.current)
+    }
+
+    let(:old_season) {
+      create(:season,
+             created_at: 1.day.ago,
+             episodes: [old_episode, new_episode])
+    }
+    let(:new_season) {
+      create(:season,
+             created_at: Time.current)
+    }
+
+    before do
+      new_season
+      old_season
+      get_request
+    end
+
+    it 'returns list of seasons ordered by creation' do
+      expect(parse_json(response.body, 'data/0/id')).to eq(old_season.id.to_s)
+      expect(parse_json(response.body, 'data/1/id')).to eq(new_season.id.to_s)
+    end
+
+    it 'returns list of associated episodes ordered by creation' do
+      expect(parse_json(response.body, 'data/0/relationships/episodes/data/0/id')).to eq(old_episode.id.to_s)
+      expect(parse_json(response.body, 'data/0/relationships/episodes/data/1/id')).to eq(new_episode.id.to_s)
+    end
+
     it { is_expected.to eq 200 }
   end
 end

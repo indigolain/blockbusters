9fb1b017dec2793c04a01913ca7d7db33f183113
indigolain <indigo.lain@gmail.com>
2019-09-16 04:26:58 +0900

    Add spec for testing out order of visual contents

--- spec/requests/visual_contents_controller_spec.rb
+++ spec/requests/visual_contents_controller_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe VisualContentsController, type: :request do
   describe 'GET #index' do
     subject(:get_request) { get visual_contents_path, headers: request_header }
 
+    let(:old_visual_content) { create(:visual_content, created_at: 1.day.ago) }
+    let(:new_visual_content) { create(:visual_content, created_at: Time.current) }
+
     it { is_expected.to eq 200 }
+
+    before do
+      new_visual_content
+      old_visual_content
+      get_request
+    end
+
+    it { is_expected.to eq 200 }
+    it 'returns list of visual_contents ordered by creation' do
+      expect(parse_json(response.body, 'data/0/id')).to eq(old_visual_content.id.to_s)
+      expect(parse_json(response.body, 'data/1/id')).to eq(new_visual_content.id.to_s)
+    end
   end
 end
